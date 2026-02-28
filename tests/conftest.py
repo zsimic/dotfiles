907cc2a02e99de03e646654279b040511422b611
Zoran Simic <zoran@simicweb.com>
2026-02-27 17:59:43 -0800

    Use chezmoiroot

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from pathlib import Path
 from runez.conftest import cli  # noqa: F401, fixture
 
 # Temp hack: this allows to import `.py` files from the `bin/` folder
-PROJECT_PATH = Path(__file__).parent.parent / "bin"
+PROJECT_PATH = Path(__file__).parent.parent / "home/bin"
 sys.path.insert(0, str(PROJECT_PATH))
