d9bb41e80b1af0cf14942f9091302d268f84bde0
Zoran Simic <zoran@simicweb.com>
2026-03-01 15:42:09 -0800

    Fine tuned bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,11 @@ typeset -U path
 path+=(/opt/homebrew/bin)
 path+=(/home/linuxbrew/.linuxbrew/bin)
 
-if ! command -v brew > /dev/null; then
-    NONINTERACTIVE=1 $SHELL -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    hash -r
+if ! command -v chezmoi > /dev/null; then
+    if ! command -v brew > /dev/null; then
+        NONINTERACTIVE=1 $SHELL -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        hash -r
+    fi
     brew install chezmoi
 fi
 
