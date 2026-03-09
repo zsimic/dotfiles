a6b5b8b510328670fa1c51804a648f1787997dc8
Zoran Simic <zsimic@netflix.com>
2026-03-09 11:30:04 -0700

    Don't assume zsh for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Bootstrap chezmoi
 # zsh -c "$(curl -fsSL https://raw.githubusercontent.com/zsimic/dotfiles/main/bootstrap.sh)"
 
-set -eufx
-typeset -U path
-path=(/opt/homebrew/bin /home/linuxbrew/.linuxbrew/bin $path)
+set -ex
+
+PATH="/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:$PATH"
 
 if ! command -v brew > /dev/null; then
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     for folder in $path[1,2]; do
-        if [[ -x "$folder/brew" ]]; then
+        if [ -x "$folder/brew" ]; then
             eval "$($folder/brew shellenv)"
             break
         fi
